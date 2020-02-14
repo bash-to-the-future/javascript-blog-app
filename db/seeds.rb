d62d07c10e872c8309c6c748e2ef51d26f3eb4c4
JackMarx <seriousfools@gmail.com>
2020-02-14 12:31:19 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+
+Post.create([
+  { title: 'Star Wars' , body: "The first three films are amazing, the next three don't exist in my mind, last three are ok"}, 
+  { title: 'Lord of the Rings', body: "It took me a long time to enjoy these films" }
+])
+
---
+++ public/index.js
@@ -0,0 +1,8 @@
+console.log("Yo");
+
+axios
+  .get("http://localhost:3000/api/posts")
+  .then(function(response) {
+    var posts = response.data;
+    console.log(posts);
+  });
